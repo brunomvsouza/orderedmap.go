fb1d11530e9550e794a6e5c539f903be9b1c5a16
Bruno M V Souza <hi@brunosouza.org>
2018-08-11 12:18:44 +0200

    Update licence and package description

--- orderedmap.go
+++ orderedmap.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2018, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
+// Package orderedmap implements a thread safe insertion ordered map.
 package orderedmap // import "go.bmvs.io/orderedmap"
 
 import (
